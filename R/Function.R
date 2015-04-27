3b0228254961c71b50210cd63f5c0c9728103294
Lian Lian <lianlian@epi53.epidemiology.epi.msu.edu>
2015-04-27 18:02:40 -0400

    add function colmult

--- R/Function.R
+++ R/Function.R
@@ -1,3 +1,17 @@
 ##functions
 #Impute marker genotypes by column mean
 meanImpute=function(X){apply(X,2,function(a){if(any(is.na(a))){a[which(ia.na(a))]=mean(a)};return(a)})
+	
+#Get columwise multiplication 
+colmult=function(Z1,Z2){
+		p2=ncol(Z1)*ncol(Z2)
+	Z3=matrix(nrow=nrow(Z1),ncol=p2)
+
+	for(j in 1:ncol(Z2)){
+	for(i in c(1:ncol(Z1))){
+	Z3[,(j-1)*ncol(Z1)+i]=Z1[,i]*Z2[,j]	
+		}
+	}
+	return(Z3)
+	}	
+	
