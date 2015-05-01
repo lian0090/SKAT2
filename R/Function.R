dd389376ff5f20e139c7b3c0d6999f2a217acb1c
lian0090 <lian0090@umn.edu>
2015-05-01 09:53:29 -0400

    changed sweep Cgamma

--- R/Function.R
+++ R/Function.R
@@ -66,3 +66,91 @@ out=neg2Log(Var=Var,tU1y=tU1y,tU1X=tU1X,tXX=tXX,tXy=tXy,tyy=tyy,d1=d1,n=n,tU1W=t
 out=-1/2*out
 return(out)
 }
+
+##simulate power and size for GxE
+simuPower=function(geno,snp.id=NULL,N,eigenG,mu,var_e,kg=0,ks=0.2,kx=0,nsets=100,winsize=30,seed=1,nQTL=100,Xf,Xe=NULL,SKAT=T,Score=T,LR=F,alpha=0.001,saveAt=NULL){
+  
+  #geno:  matrix, or gds.class object
+  #snp.id: names of SNPs, must be specified for gds object
+  #N size of population, 
+  #eigenG: eigen decoposition for G matrix
+  #winsize: windowsize 
+  #nQTL: number of QTLs in the genetic background
+  #Xf: fixed effect (not included in GxE)
+  #Xe: fixed effect (included for GxE) 
+  #alpha: test size
+  set.seed(seed)
+  
+  if(is.null(saveAt)){
+    saveAt=paste("ks",ks,".dat",sep="")
+  }
+  
+  if(file.exists(saveAt)){
+    stop(saveAt, "exists in disk, please specify a new name for save file")
+  }
+  cat("#","mu=",mu, "var_e=",var_e, "kg=",kg,"ks=",ks, "kx=",kx, "nsets=",nsets, "winsize=", winsize,"nQTL=",nQTL, "alpha=",alpha,"\n",file=saveAt,append=F)
+  
+  if("gds.class" %in% class(geno)){
+    if(! "gdsfmt" %in% rownames(installed.packages())) stop("must install gdsfmt package to use gds.class file")
+    if(is.null(snp.id)){stop("must specify snp.id for gds object")}
+    p=length(snp.id)
+    bcQTLs=sample(1:p,100)
+    Zgsnps=snp.id[bcQTLs]
+    Zg=snpgdsGetGeno(genofile,sample.id=NULL,snp.id=Zgsnps,snpfirstdim=F)
+    
+  }
+  if("matrix" %in% class(geno)){
+    p=ncol(geno)
+    bcQTLs=sample(1:p,100)
+    Zg=geno[,bcQTLs]
+  }
+  X=cbind(Xf,Xe)
+  beta_x=rep(0.5/ncol(X),ncol(X))
+  beta_g=kg*var_e
+  Zg_Z_u=simuBeta(Zg,kg,var_e)
+  rm("Zg")
+  
+  tnsets=ceiling(p/winsize)
+  
+  if(is.null(nsets)){
+    nsets=tnsets
+    sets=c(1:nsets)
+  }else{
+    sets=sample(1:nsets,nsets,replace=F)
+  }
+  for(i in 1:nsets) {
+    seti=sets[i]
+    if("gds.class" %in% class(geno)){
+      Zs=read.gdsn(index.gdsn(genofile, "genotype"), start=c(1,(seti-1)*winsize+1), count=c(-1,min(winsize,p-winsize*seti)))	
+    }	
+    if("matrix" %in% class(geno)){
+      Zs=geno[,seq((seti-1)*winsize+1,length.out=min(winsize,p-winsize*seti))]
+    }
+    Zs_Z_u=simuBeta(Zs,ks,var_e) 
+    rm("Zs")
+    #GxE
+    Zx=colmult(Xp,Zs_Z_u$Z)
+    Zx_Z_u=simuBeta(Zx,kx,var_e)
+    rm("Zx")	
+    e=rnorm(N,0,sqrt(var_e))
+    y=X%*%beta_x+Zg_Z_u$u+Zs_Z_u$u+Zx_Z_u$u+e
+    ptm=proc.time()[3]
+    out=testZ(y=y,X=cbind(1,Xp),W=cbind(Zs_Z_u$Z),kw=c(ncol(Zs_Z_u$Z)),Zt=Zx_Z_u$Z,eigenZd=eigenG,SKAT=T,Score=T,LR=F)
+    ptm2=proc.time()[3]
+    if(SKAT==T){
+      cat(out$p.SKAT$p.value,"\t",file=saveAt,append=T)
+    }
+    if(Score==T){
+      cat(out$p.Score,"\t",file=saveAt,append=T)
+    }
+    if(LR==T){
+      cat(out$p.LR,"\t",file=saveAt,append=T)
+    }
+    cat("\n",file=saveAt,append=T)
+    used.time=ptm2-ptm
+    cat(i,used.time,"\n")
+  }
+  p.SKAT=matrix(scan(saveAt,comment="#"),nrow=nsets,byrow=T)
+  power=apply(p.SKAT,2,mean) 
+  return(power)
+}
