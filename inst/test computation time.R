b847cd1527a4e07687a8a13d7b664d3e40b4d88f
lian0090 <lian0090@umn.edu>
2015-08-19 15:54:04 -0400

    added makevar

--- inst/test computation time.R
+++ inst/test computation time.R
@@ -1,3 +1,5 @@
+
+setwd("~/Dropbox/github/SKAT2/inst")
 library(SKAT2)
 data(mouse)
 n=1825
@@ -7,23 +9,21 @@ Z1=mouse.X[1:n,1:p]
 X=cbind(model.matrix(~pheno$GENDER)[,-1],pheno$CageDensity)
 X1=cbind(1,model.matrix(~pheno$GENDER)[,-1],pheno$CageDensity)
 y=pheno$Obesity.BMI
-G=tcrossprod(scale(mouse.X[1:n,],T,F))
-eigenG=getEigenZd(Kd=G)
-W=colmult(X,Z1)$Z
+#G=tcrossprod(scale(mouse.X[1:n,],T,F))
+#eigenG=getEigenZd(Kd=G)
+
+#save(G,file="G.rda")
+#save(eigenG,file="eigenG.rda")
+
+load("G.rda")
+load("eigenG.rda")
+
+Zt=colmult(X,Z1)$Z
 
 
 Rprof("SKATmem.out",memory.profiling = TRUE)
-out=testWindow(y,X=X1,Zt=Z1,W=list(W),eigenG=eigenG)
+out=testWindow(y,X=X1,Zt=Zt,W=list(Z1),eigenG=eigenG)
 Rprof(NULL)
 summaryRprof("SKATmem.out",memory="both")
 
 
-Rprof("GibbsI.out",memory.profiling = TRUE)
-FW(y,VAR,ENV)
-Rprof(NULL)
-summaryRprof("GibbsI.out",memory="both")
-
-Rprof("GibbsOLS.out",memory.profiling = TRUE)
-FW(y,VAR,ENV,method="OLS")
-Rprof(NULL)
-summaryRprof("GibbsI.out",memory="both")
